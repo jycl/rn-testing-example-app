fad4e53b3938661a2712a53eba676b27fcf1c6c5
Joshua Leung <joshuaycleung@gmail.com>
2018-09-12 23:28:27 +0800

    Update Detox testing file and update CI config file

--- e2e/firstTest.spec.js
+++ e2e/firstTest.spec.js
@@ -15,9 +15,4 @@ describe('Example', () => {
     await element(by.id('button')).tap();
     await expect(element(by.text("2"))).toBeVisible();
   });
-
-  it('should show world screen after tap', async () => {
-    await element(by.id('world_button')).tap();
-    await expect(element(by.text('World!!!'))).toBeVisible();
-  });
 })
